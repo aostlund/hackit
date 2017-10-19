936400bf6a2989bdbd510586bea24fc860a69740
Andreas Östlund <aost@Andreass-MacBook-Pro.local>
2017-10-19 12:37:41 +0200

    cleaned up reducers

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -11,14 +11,6 @@ export default function(state = {}, action) {
             return {...state, posts: action.payload }
         case GET_POST:
             return {...state, posts: action.payload }
-        // case CHANGE_POST_SCORE:
-        //     const index = state.posts.findIndex(post => post.id === action.payload.id)
-        //     return {...state, posts: state.posts.map((post, idx) => {
-        //         if (idx === index) {
-        //             return {...post, score: post.score += action.payload.change}
-        //         }
-        //         return post
-        //     })}
         case NUM_POSTS:
             return {...state, numPosts: action.payload}
     }
