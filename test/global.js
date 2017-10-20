95252f9e3649146cb1b2df003e944d13e89cf685
Andreas Östlund <aost@Andreass-MacBook-Pro.local>
2017-10-20 18:50:56 +0200

    removed test as they are old and incomplete

--- test/global.js
+++
@@ -1,8 +0,0 @@
-import _$ from 'jquery';
-import jsdom from 'jsdom';
-
-global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
-global.window = global.document.defaultView;
-global.navigator = global.window.navigator;
-
-global.$ = _$(global.window)
